4a367ba1e6ea695e8f2296156359916f4e027b4f
busuncle <lizhij2@gmail.com>
2013-03-25 15:11:56 +0800

    improve forward_backward on initialization

--- forward_backward.py
+++ forward_backward.py
@@ -29,17 +29,15 @@ def forward_backward(obs, states, start_p, trans_m, emit_m):
     # run forward algorithm, using markov property, we get:
     # P(State_t | Observation_0:t) = alpha * P(Observation_t | State_t) * sum(P(State_t | State_t-1) * P(State_t-1 | Observation_0:t-1) for all State in timeline t-1)
     forward_probs = []
-    f_0_to_pre_t = {}
-    for i in xrange(len(obs)):
+    # initialization, when it's in timeline 0, P(State_0 | Observation_0:0) can be the prior probability of the state
+    f_0_to_pre_t = dict((s, emit_m[s][obs[0]] * start_p[s]) for s in states)
+    forward_probs.append(f_0_to_pre_t)
+    for i in xrange(1, len(obs)):
         f_0_to_t = {}
         for s in states:
-            if i == 0:
-                # when it's in timeline 0, P(State_0 | Observation_0:0) can be the prior probability of the state
-                f_0_to_t[s] = emit_m[s][obs[i]] * start_p[s]
-            else:
-                # using the recursive formula f_1:t = P(State_t | Observation_0:t), compute f_0:t with f_0:t-1
-                # here, s0 means a state occur before s
-                f_0_to_t[s] = emit_m[s][obs[i]] * sum(trans_m[s0][s] * f_0_to_pre_t[s0] for s0 in states)
+            # using the recursive formula f_1:t = P(State_t | Observation_0:t), compute f_0:t with f_0:t-1
+            # here, s0 means a state occur before s
+            f_0_to_t[s] = emit_m[s][obs[i]] * sum(trans_m[s0][s] * f_0_to_pre_t[s0] for s0 in states)
 
         forward_probs.append(f_0_to_t)
         f_0_to_pre_t = f_0_to_t
@@ -50,18 +48,16 @@ def forward_backward(obs, states, start_p, trans_m, emit_m):
     # run backward, k required (1 <= k < t)
     # P(Observation_k+1:t | State_k) = sum(P(Observation_k+1 | State_k+1) * P(Observation_k+2:t | State_k+1) * P(State_k+1 | State_k) for all state in timeline k+1)
     backward_probs = []
-    b_post_k_to_t = {}
-    for i in xrange(len(obs), -1, -1):
+    # initialization here, i = T(the length of the sequence and exceed the max index t-1), 
+    # and then Observation_t:t-1 becomes a null sequence, its probability is 1
+    b_post_k_to_t = dict((s, 1.0) for s in states)
+    backward_probs.insert(0, b_post_k_to_t)
+    for i in xrange(len(obs) - 1, -1, -1):
         b_k_to_t = {}
         for s in states:
-            if i == len(obs):
-                # here, i = T(the length of the sequence and exceed the max index t-1), 
-                # and then Observation_t:t-1 becomes a null sequence, its probability is 1
-                b_k_to_t[s] = 1.0
-            else:
-                # using the recursive formula b_k+1:t = P(Observation_k+1:t | State_k), compute b_k+1:t with b_k+2:t
-                # here, s1 means a state occur after s
-                b_k_to_t[s] = sum(emit_m[s1][obs[i]] * b_post_k_to_t[s1] * trans_m[s][s1] for s1 in states)
+            # using the recursive formula b_k+1:t = P(Observation_k+1:t | State_k), compute b_k+1:t with b_k+2:t
+            # here, s1 means a state occur after s
+            b_k_to_t[s] = sum(emit_m[s1][obs[i]] * b_post_k_to_t[s1] * trans_m[s][s1] for s1 in states)
             
         backward_probs.insert(0, b_k_to_t)
         b_post_k_to_t = b_k_to_t
